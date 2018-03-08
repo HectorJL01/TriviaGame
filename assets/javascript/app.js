e7353b956f38d2108b96e3a6ad5519dc6777ceee
Héctor Lopez <hectorjl01@hectors-mbp.home>
2018-03-08 16:01:37 -0500

    rev7

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,8 +7,9 @@ $(document).ready(function () {
         
     })
     var youreScore = 0;
-    var userselection = [];
-    var correctAnswer = [];
+    var userselection = ["t","f"];
+    var correctAnswer = ["t"];
+    var wrongAnswer = ["f"]
     var wins = 0;
     var losses = 0;
     var questionArray= [];
@@ -41,41 +42,43 @@ $(document).ready(function () {
         function triviaQuestions() {
             questionArray = [{
             question: "1. Which is the capital of Australia?",
-            answers: ("Canberra", "Sydney", "Melbourne", "Perth"),
+            answers: ["Canberra", "Sydney", "Melbourne", "Perth"],
             correctAnswer: 0
         },
         {
-            question: "2.In what year the breaking of the Berlin Wall began?",
+            question: "2. In what year the breaking of the Berlin Wall began?",
             answers: ["1989", "1990", "1987", "1980"],
             correctAnswer: 0
         },
         {
-            question: "3.Where the Sistine Chapel is located:",
+            question: "3. Where the Sistine Chapel is located:",
             answers: ["Rome", "France", "Vatican City", "Budapest"],
             correctAnswer: 2
         },
         {
-            question: "The Prime Minister of Canada is called?",
+            question: "4. The Prime Minister of Canada is called?",
             answers: ["Angela Merkel", "Theresa May", "Justin Trudeau", "Stefan Lofven"],
             correctAnswer: 2
         },
         {
-            question: "5.How many oceans has the planet",
+            question: "5. How many oceans has the planet",
             answers: ["6", "7", "4", "5"],
             correctAnswer: 3
         },
         ]
 
+        document.getElementById(answers)
+        console.log(answers)
         $("#question0").html(questionArray[0].question);
-        $("answers").html(questionArray.answers);
+        $("input#answers").html(questionArray[0].answers[0]);
         $("#question1").html(questionArray[1].question);
-        // $("#answers").html(questionArray[1].answers);
+        // $("#answers").html(questionArray[].answers);
         $("#question2").html(questionArray[2].question);
-        // $("#answers").html(questionArray[2].answers);
+        // $("#answers").html(questionArray[].answers);
         $("#question3").html(questionArray[3].question);
-        // $("#answers").html(questionArray[3].answers);
+        // $("#answers").html(questionArray[].answers);
         $("#question4").html(questionArray[4].question);
-        // $("#answers").html(questionArray[0].answers);
+        // $("#answers").html(questionArray[].answers);
 
 
         function correct (){
@@ -86,7 +89,7 @@ $(document).ready(function () {
 
         }
         function incorrect (){
-            if (userselection === incorrect)
+            if (userselection === wrongAnswer)
             losses ++;
             $('#losses').text(losses);
 
